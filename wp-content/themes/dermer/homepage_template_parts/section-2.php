db5d736a8142d5ac771d89f4b72dfc21a92f4060
Garrett Cullen <garrettwcullen@gmail.com>
2018-07-17 15:27:33 -0700

    push

---
+++ wp-content/themes/dermer/homepage_template_parts/section-2.php
@@ -0,0 +1,64 @@
+<section id="section_two">
+	
+	<div class="sec_two_inner">
+		
+		
+		<div class="sec_two_slider">
+			
+			
+			<div class="single_slide">
+				
+				<div class="single_slide_inner">
+				
+					<span class="small_header">Over 80 Years of</span><!-- small_header -->
+				
+					<span class="large_header">Experience</span><!-- large_header -->
+					
+						<span class="slide_content">We know how the insurance industry functions and how to maximize results for our injured clients. Should your case be tried by a jury, we are more than confident in the courtroom.</span>
+							
+					
+					</div><!-- single_slide_inner -->
+				
+			</div><!-- single_slide -->
+			
+			<div class="single_slide">
+				
+				<div class="single_slide_inner">
+				
+					<span class="small_header">Over 80 Years of</span><!-- small_header -->
+				
+					<span class="large_header">Experience</span><!-- large_header -->
+					
+						<span class="slide_content">We know how the insurance industry functions and how to maximize results for our injured clients. Should your case be tried by a jury, we are more than confident in the courtroom.</span>
+							
+					
+					</div><!-- single_slide_inner -->
+				
+			</div><!-- single_slide -->
+			
+			<div class="single_slide">
+				
+				<div class="single_slide_inner">
+				
+					<span class="small_header">Over 80 Years of</span><!-- small_header -->
+				
+					<span class="large_header">Experience</span><!-- large_header -->
+					
+						<span class="slide_content">We know how the insurance industry functions and how to maximize results for our injured clients. Should your case be tried by a jury, we are more than confident in the courtroom.</span>
+							
+					
+					</div><!-- single_slide_inner -->
+				
+			</div><!-- single_slide -->
+			
+			
+			
+			
+			
+		</div><!-- sec_two_slider -->
+		
+		
+	</div><!-- sec_two_inner -->
+	
+	
+</section><!-- section_two -->
