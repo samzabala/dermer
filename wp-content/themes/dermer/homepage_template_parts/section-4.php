e240c56cc9ec82618c414bca95b1304889a4dc8e
Garrett Cullen <garrettwcullen@gmail.com>
2018-07-19 15:31:59 -0700

    push

---
+++ wp-content/themes/dermer/homepage_template_parts/section-4.php
@@ -0,0 +1,26 @@
+<section id="section_four">
+	
+	<div class="sec_four_inner">
+		
+		<div class="sec_four_left">
+			
+			<span class="small_header">former insurance defense attorneys</span><!-- small_header -->
+			
+			<span class="sec_four_intro">Our attorneys spent years working as insurance defense attorneys and now put that experience to use fighting for the best results for our clients.</span><!-- sec_four_intro -->
+			
+			<div class="sec_four_content content">
+				
+				<h2>Nam convallis ex nec dictum ultrices lorem purus hendrerit ex ultrices sollicitudin massa mauris ut elit.</h2>
+				
+				<p>Phasellus sit met auctor velit, ac egestas augue. Donec sollicitudin pharetra ma na sed varius. Nam convallis, ex nec dictum ultrices, lorem purus hendrerit ex, ultrices sollicitudin massa mauris ut elit. Sed pharetra tellus quis nisl scelerisque elementum a quis odio. Integer porta quam purus, id porttitor quam fermentum eget. Vestibulum iaculis ligula sit amet tincidunt placerat. Pellentesque ac elit sit amet tortor scelerisque fringilla. Pellentesque ut dolor tortor. Aliquam tristique dictum risus in viverra. Nullam sit amet scelerisque libero. Nam mollis consectetur malesuada. Donec ut nisl nec risus consequat facilisis. Phasellus id dui sollicitudin, faucibus libero et, hendrerit libero. Cras fermentum nunc ligula, sed lacinia arcu aliquet eu. vOrci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris molestie imperdiet quam sit amet scelerisque. Nulla nec nisi vitae sem tempor consectetur non a neque. iaculis quis. Donec nisl ipsum, elementum lobortis ipsum non, eleifend venenatis massa. Ut rhoncus auctor nisl, quis efficitur ex consequat sed. Vivamus ligula neque, lacinia sit amet magna vel, elementum semper orci. Sed sollicitudin libero a odio dignissim egestas. Phasellus enim enim, egestas vitae libero sed, bibendum vestibulum libero. Aliquam sed massa a dolor venenatis pulvinar.</p>
+				
+			</div><!-- sec_four_content -->
+			
+		</div><!-- sec_four_left -->
+		
+		<div class="sec_four_right"></div><!-- sec_four_right -->
+		
+	</div><!-- sec_four_inner -->
+	
+	
+</section><!-- section_four -->
--- wp-content/themes/dermer/page-home.php
+++ wp-content/themes/dermer/page-home.php
@@ -9,6 +9,7 @@
 <?php get_template_part( 'homepage_template_parts/section', '1' );?>
 <?php get_template_part( 'homepage_template_parts/section', '2' );?>
 <?php get_template_part( 'homepage_template_parts/section', '3' );?>
+<?php get_template_part( 'homepage_template_parts/section', '4' );?>
 
 
 <?php get_footer(); ?>
