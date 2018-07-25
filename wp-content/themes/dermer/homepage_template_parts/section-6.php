0722af6ac699290b28a3bcac8762977125f21bf8
Garrett Cullen <garrettwcullen@gmail.com>
2018-07-25 09:42:39 -0700

    push

--- wp-content/themes/dermer/homepage_template_parts/section-6.php
+++ wp-content/themes/dermer/homepage_template_parts/section-6.php
@@ -1,6 +1,36 @@
 <section id="section_six">
 	
+	<div class="sec_six_inner">
 	
+	<div class="sec_six_left">
+		
+		<div class="sec_six_slider">
+			
+			<div class="sec_six_single_slide">
+				
+				<span class="stars_svg"><?php echo file_get_contents("wp-content/themes/dermer/images/icon-stars.svg"); ?></span><!-- stars_svg -->
+				
+				<span class="sec_six_content">“I did not know where to turn legally, until I met Kim Ruder and Daimon Carter, my earthly angels!  Kim and Daimon told me to heal and leave the legal stuff to them and I did.”</span><!-- sec_six_content -->
+				
+				<span class="sec_six_name">Nicole C.</span><!-- sec_six_name -->
+				
+			</div><!-- sec_six_single_slide -->
+			
+			<div class="sec_six_buttons">
+				
+				<span class="left_button sec_six_button"><?php echo file_get_contents("wp-content/themes/dermer/images/new_slide_button.svg"); ?></span><!-- left_button -->
+				
+				<span class="right_button sec_six_button"><?php echo file_get_contents("wp-content/themes/dermer/images/new_slide_button.svg"); ?></span><!-- right_button -->
+				
+			</div><!-- sec_six_buttons -->
+			
+		</div><!-- sec_six_slider -->
+		
+	</div><!-- sec_six_left -->
+	
+	<div class="sec_six_right"></div><!-- sec_six_right -->
+	
+	</div><!-- sec_six_inner -->
 	
 	
 </section><!-- section_six -->
