d0cee51dbdee5f20b5b7f80e6f939b6ab76a6bcf
Garrett Cullen <Garth619@users.noreply.github.com>
2018-07-29 12:00:44 -0700

    push

--- wp-content/themes/dermer/footer.php
+++ wp-content/themes/dermer/footer.php
@@ -1,4 +1,16 @@
-
+<footer>
+	
+	
+	<div class="footer_left">
+		
+		<span class="large_header">We work for you</span><!-- large_header -->
+		
+	</div><!-- footer_left -->
+	
+	<div class="footer_right"></div><!-- footer_right -->
+	
+	
+</footer>
 
 <?php wp_footer();?>
 
