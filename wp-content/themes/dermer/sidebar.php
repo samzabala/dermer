dbef2c74c8b8bc9b1dd1b63db5ba219b48a7e11b
Garrett Cullen <garrettwcullen@gmail.com>
2018-07-30 14:34:26 -0700

    push

--- wp-content/themes/dermer/sidebar.php
+++ wp-content/themes/dermer/sidebar.php
@@ -6,8 +6,21 @@
 		
 		<span class="free_case_title">request a free case evaluation</span><!-- free_case_title -->
 		
+		<?php gravity_form(3, false, false, false, '', true, 224); ?>
+		
+		<span class="required">Fields Required</span><!-- required -->
+		
 		
 	</div><!-- sidebar_form -->
+	
+	<div class="sidebar_list">
+		
+		
+		<ul>
+			
+		</ul>
+		
+	</div><!-- sidebar_list -->
 	
 	
 </div><!-- sidebar_wrapper -->
