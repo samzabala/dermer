bdb5e217f4a1b98c157133c8f053cecd157b5512
Garrett Cullen <garrettwcullen@gmail.com>
2018-08-02 13:26:02 -0700

    push

--- wp-content/themes/dermer/page-testimonials.php
+++ wp-content/themes/dermer/page-testimonials.php
@@ -31,10 +31,87 @@
 					
 				</div><!-- single_test -->
 				
+				<div class="single_test">
+					
+					<span class="stars"><?php echo file_get_contents("wp-content/themes/dermer/images/new_icon_stars.svg"); ?></span><!-- stars -->
+					
+					<span class="testi_intro">“Mr Dermer walked me through every step of the process and never made me feel as if I was asking too many questions about my case.”</span><!-- testi_intro -->
+					
+					<div class="testi_content">
+						
+						<p>When I was injured and my car was totaled in a car accident that I was not at fault for, I was so thankful that the other driver had car insurance . I assumed that they would have my best interests at heart. I had numerous medical bills, six months of physical therapy, along with the total loss of my car. Hearing my frustration in trying to deal with the auto insurance company, a close family friend referred me to Stephen Dermer.</p>
+
+<p>I had never needed the services of a lawyer and didn’t know quite what to expect. Mr. Dermer took the whole situation off my shoulders. He immediately became the contact person in dealing with the insurance company, which freed me up to focus on my physical recovery from the accident. He was very professional, respectful and compassionate. Mr Dermer walked me through every step of the process and never made me feel as if I was asking too many questions about my case. He promptly answered every phone call/text and kept me up to date with every aspect of my case.</p> 
+
+<p>I felt a bit overwhelmed by my medical bills, and Mr. Dermer not only made every effort to reassure me, but also took care of gathering all of my medical records needed for my case.</p>
+
+<p>My worst fear was that we were going to have to take my case to court. Mr. Dermer was able to settle my case in mediation and I was extremely happy with the outcome. Stephen Dermer helped me to navigate through one of the most stressful situations in my life and I highly recommend him.</p>
+						
+					</div><!-- testi_content -->
+					
+					<span class="name">Jennifer F.</span><!-- name -->
+					
+				</div><!-- single_test -->
+			
+				
 			</div><!-- testi_col -->
 			
 			<div class="testi_col">
 				
+				<div class="single_test">
+					
+					<span class="stars"><?php echo file_get_contents("wp-content/themes/dermer/images/new_icon_stars.svg"); ?></span><!-- stars -->
+					
+					<span class="testi_intro">“I did not know where to turn legally, until I met Kim Ruder and Daimon Carter, my earthly angels!  Kim and Daimon told me to heal and leave the legal stuff to them and I did.”</span><!-- testi_intro -->
+					
+					<div class="testi_content">
+						
+						<p>July 15, 2017 was a horrible day for me...a motorcycle accident left me with a severely broken arm, that needed emergency surgery steel plates, screws and months of rehab. Not only was I in distress physically, but I did not know where to turn legally, until I met Kim Ruder and Daimon Carter, my earthly angels! Kim and Daimon told me to heal and leave the legal stuff to them and I did.</p>
+
+<p>They were not only my legal representatives, they became my friends in guiding me through the insurance process from the accident and advising me for my future. Kim and Daimon were always available when I needed to ask a question or had a concern and ensured that I was comfortable with the answer. Once I told my mom how Daimon was able to locate an additional settlement for me, my mom said to me "you have some very smart lawyers on your side." I told her YES you are correct!!!!  I have no clue financially where I would be without Kim and Daimon, thank you guys, there are no words that can express my gratitude!!! </p>
+						
+					</div><!-- testi_content -->
+					
+					<span class="name">Nicole C.</span><!-- name -->
+					
+				</div><!-- single_test -->
+				
+				<div class="single_test">
+					
+					<span class="stars"><?php echo file_get_contents("wp-content/themes/dermer/images/new_icon_stars.svg"); ?></span><!-- stars -->
+					
+					<span class="testi_intro">“Mr. Dermer knows a lot of people in the business and has the experience necessary to obtain an equitable settlement.”</span><!-- testi_intro -->
+					
+					<div class="testi_content">
+						
+						<p>Three weeks after having major intestinal surgery, I was involved in a significant auto accident as I was returning home from my doctor's office. The accident resulted in a variety of complications, including fluid build up, bleeding and colon leaks.  As a result, additional surgeries and procedures were required.</p>
+
+						<p>Mr. Dermer was able to work with my physicians to build a case against the insurance company, forcing the insurance company to settle my lawsuit without having to go to court. Mr. Dermer kept me informed throughout the legal process and I felt like I knew what was going on with my case every step of the way. Mr. Dermer was referred to me by a well respected attorney that represents big insurance companies.</p>
+						
+					</div><!-- testi_content -->
+					
+					<span class="name">Anonymous</span><!-- name -->
+					
+				</div><!-- single_test -->
+				
+				
+				<div class="single_test">
+					
+					<span class="stars"><?php echo file_get_contents("wp-content/themes/dermer/images/new_icon_stars.svg"); ?></span><!-- stars -->
+					
+					<span class="testi_intro">“I would recommend Mr. Appel's law firm because they will treat you with respect and with high regard.”</span><!-- testi_intro -->
+					
+					<div class="testi_content">
+						
+						<p>I was looking for a dedicated lawyer that I knew could understand my situation and would be committed to helping me resolve my case. I heard about Mr. Appel's law firm through a family member and once I met him in person, he reassured me and eased me through the process. Throughout this long process, Adam Appel was in constant contact with me and informed me about what was happening throughout the case. He was a source of confidence and guided me through a very overwhelming situation.</p>
+
+						<p>He aided me in various ways such as recommending other sources of assistance. He listened to my story, understood my situation and was very dedicated to helping me resolve my case. Mr. Appel works tirelessly and efficiently. He gets the job done in a very professional manner. I would recommend Mr. Appel's law firm because they will treat you with respect and with high regard. If I had to choose a law firm, I would go back to DAR Law. </p>
+						
+					</div><!-- testi_content -->
+					
+					<span class="name">Anonymous</span><!-- name -->
+					
+				</div><!-- single_test -->
 				
 				
 			</div><!-- testi_col -->
