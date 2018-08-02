57ffc4845b55ed6550fc32a7e047170904a40661
Garrett Cullen <garrettwcullen@gmail.com>
2018-08-02 11:31:31 -0700

    push

---
+++ wp-content/themes/dermer/page-testimonials.php
@@ -0,0 +1,55 @@
+<?php
+
+/* Template Name: Testimonials */	
+	
+ get_header(); ?>
+
+
+<div class="inner_main pattern">
+	
+		<h1 class="centered"><?php the_title();?></h1><!-- page_title -->
+		
+		<div class="testimonial_wrapper">
+			
+			<div class="testi_col">
+				
+				<div class="single_test">
+					
+					<span class="stars"><?php echo file_get_contents("wp-content/themes/dermer/images/new_icon_stars.svg"); ?></span><!-- stars -->
+					
+					<span class="testi_intro">“Mr. Appel was quick to respond to my calls or emails, and I never worried about my case being neglected. ”</span><!-- testi_intro -->
+					
+					<div class="testi_content">
+						
+						<p>I was involved in a terrible automobile accident last year, hit by a truck while driving on I285. My car was totaled. I was unconscious and taken to a hospital by ambulance. While in the hospital I asked a friend for advice. She suggested her lawyer, Mr. Adam Appel. She told me that he was voted a super lawyer of Georgia many times and that she is always pleased with his handling of her problems. I called him and we spoke at length. I felt very confident that he would take care of my needs and that I was in capable hands. And that’s exactly how everything went for me.</p>
+
+						<p>Mr Appel explained each step along the way to a resolution. And I was relieved to be able to concentrate on my recovery, without having to be involved with the trucking firm whose driver hit me. Mr. Appel was quick to respond to my calls or emails, and I never worried about my case being neglected. And it was settled in a reasonable length of time in a most satisfactory way. For all these reasons, I would feel confident in recommending Mr. Adam Appel and his firm to anyone who values competence along with peace of mind.</p>
+						
+					</div><!-- testi_content -->
+					
+					<span class="name">Roslin W.</span><!-- name -->
+					
+				</div><!-- single_test -->
+				
+			</div><!-- testi_col -->
+			
+			<div class="testi_col">
+				
+				
+				
+			</div><!-- testi_col -->
+			
+		</div><!-- testimonial_wrapper -->
+	
+</div><!-- inner_main -->
+
+<?php get_footer(); ?>
+
+
+
+
+
+
+	
+
+
