2e044f43efe44831e21d0272407be39e836a06b2
Garrett Cullen <garrettwcullen@gmail.com>
2018-08-02 15:43:07 -0700

    push

--- wp-content/themes/dermer/404.php
+++ wp-content/themes/dermer/404.php
@@ -1,12 +1,19 @@
 <?php get_header(); ?>
 
 	
-				
-<h1 class="entry-title">Not Found</h1>
-				
-				
-<p>Apologies, but the page you requested could not be found.</p>
-				
-<?php // get_sidebar(); ?>
+<div class="inner_main">
+	 
+	 <div class="not_found_wrapper">
+	 
+		 <h1 class="not_found">404</h1>
+	 
+		 <span class="large_header">The Page you are looking for could not be found</span><!-- large_header -->
+	 
+		 <span class="small_header">Click Here to Return to Our <a href="">Homepage</a></span><!-- small_header -->
+	 
+	 </div><!-- not_found_wrapper -->
+	 
+	 
+ </div><!-- inner_main -->
 			
 <?php get_footer(); ?>
