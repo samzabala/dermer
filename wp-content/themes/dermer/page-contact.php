d0201f34b6f144edc440bb928fbc960fc1ba43cb
Garrett Cullen <garrettwcullen@gmail.com>
2018-08-03 10:28:57 -0700

    push

--- wp-content/themes/dermer/page-contact.php
+++ wp-content/themes/dermer/page-contact.php
@@ -62,9 +62,17 @@
 		
 		
 	</div><!-- contact_wrapper -->
+	
+	<div class="iframe_wrapper">
+		
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3309.540832069643!2d-84.24032208478762!3d33.952936080632945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5047762fd5d03%3A0x5f712d602daaa762!2sDermer+Appel+Ruder+Attorneys+At+Law!5e0!3m2!1sen!2sus!4v1533314606923" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		
+		
+	</div><!-- iframe_wrapper -->
 
 	
 </div><!-- inner_main -->
+
 
 <?php get_footer(); ?>
 
