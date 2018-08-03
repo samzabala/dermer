2dfacff54b18534d21f2369be4b86770ec3e422a
Garrett Cullen <garrettwcullen@gmail.com>
2018-08-03 09:39:28 -0700

    push

---
+++ wp-content/themes/dermer/page-contact.php
@@ -0,0 +1,78 @@
+<?php 
+	
+	
+	/* Template Name: Contact */
+	
+	get_header(); ?>
+
+
+<div class="inner_main">
+	
+	<div class="contact_wrapper">
+		
+		
+		<div class="contact_wrapper_inner">
+		
+		
+			<div class="contact_left">
+				
+				<h1 class="contact_title">Contact Our Firm</h1>
+				
+			</div><!-- contact_left -->
+			
+			<div class="contact_right">
+				
+				
+				<span class="contact_svg"><?php echo file_get_contents("wp-content/themes/dermer/images/new_footer_logo.svg"); ?></span>
+				
+					<div class="contact_left_col">
+				
+						<div class="contact_flex_left">
+							
+							<span class="small_header">dermer appel ruder LLC</span><!-- large_header -->
+							
+							<a class="numbers" href="tel:(404) 892-8884"><span>P</span>(404) 892-8884</a><!-- numbers -->
+							
+							<span class="numbers"><span>f</span>(404) 892-8884</span><!-- numbers -->
+							
+							<a class="address" href="">6075 The Corners Parkway, <br class="mobile" /> Suite 210<br/> Peachtree Corners, GA 30092</a>
+							
+							<a class="google_maps" href="" target="_blank">Google Maps</a><!-- google_maps -->
+							
+						</div><!-- contact_flex_left -->
+						
+						<div class="contact_flex_right">
+							
+							<span class="small_header right">stay connected</span><!-- large_header -->
+							
+							<ul class="social">
+								<li><a href="">Google +</a></li>
+								<li><a href="">Facebook</a></li>
+								<li><a href="">Linked In</a></li>
+							</ul>
+							
+						</div><!-- contact_flex_right -->
+				
+					</div><!-- contact_left_col -->
+				
+				
+			</div><!-- contact_right -->
+			
+		</div><!-- contact_wrapper_inner -->
+		
+		
+	</div><!-- contact_wrapper -->
+
+	
+</div><!-- inner_main -->
+
+<?php get_footer(); ?>
+
+
+
+
+
+
+	
+
+
