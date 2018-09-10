4d845463e4eec326776064b4f8f5e79e654bad43
Garrett Cullen <garrettwcullen@gmail.com>
2018-09-10 10:25:57 -0700

    push

--- wp-content/themes/dermer/functions.php
+++ wp-content/themes/dermer/functions.php
@@ -21,7 +21,7 @@ function my_jquery_enqueue() {
 
  function load_my_styles_scripts() {
      // Load my stylesheet
-    //wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', '', 5, 'all' ); 
+    wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', '', 5, 'all' ); 
 
      // Load my javascripts
      wp_enqueue_script( 'jquery-addon', get_template_directory_uri() . '/js/custom-min.js', array('jquery'), '', true );
@@ -269,6 +269,7 @@ function wpbeginner_numeric_posts_nav() {
 
 
 
+/*
 
 add_action( 'wp_head', 'internal_css_print' );
 function internal_css_print() {
@@ -278,6 +279,7 @@ function internal_css_print() {
   
    echo '</style>';
 }
+*/
 
 
 
